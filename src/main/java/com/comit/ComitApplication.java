01cd84e6a4f2ea241b832bfc220b0af8852257e8
Enes Bahadır YILDIRIM <enesbahadiryildirim@gmail.com>
2020-11-04 12:11:57 +0300

    Initial commit

---
+++ src/main/java/com/comit/ComitApplication.java
@@ -0,0 +1,13 @@
+package com.comit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitApplication.class, args);
+	}
+
+}
